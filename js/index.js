085d3fd41c270487f59dd994edf4997ef4900ed8
Nathan Ertner <nathan@ertner.us>
2013-02-19 10:19:46 -0800

    added example code, changed name etc.

--- js/index.js
+++ js/index.js
@@ -17,18 +17,18 @@
  * under the License.
  */
 var app = {
-    initialize: function() {
+    initialize: function () {
         this.bind();
     },
-    bind: function() {
+    bind: function () {
         document.addEventListener('deviceready', this.deviceready, false);
     },
-    deviceready: function() {
+    deviceready: function () {
         // This is an event handler function, which means the scope is the event.
         // So, we must explicitly called `app.report()` instead of `this.report()`.
         app.report('deviceready');
     },
-    report: function(id) {
+    report: function (id) {
         // Report the event in the console
         console.log("Report: " + id);
 
@@ -40,3 +40,35 @@ var app = {
         completeElem.className = completeElem.className.split('hide').join('');
     }
 };
+
+
+// Wait for Cordova to load
+//
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// Cordova is ready
+//
+function onDeviceReady() {
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
+
+// onSuccess Geolocation
+//
+function onSuccess(position) {
+    var element = document.getElementById('geolocation');
+    element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' +
+        'Longitude: ' + position.coords.longitude + '<br />' +
+        'Altitude: ' + position.coords.altitude + '<br />' +
+        'Accuracy: ' + position.coords.accuracy + '<br />' +
+        'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '<br />' +
+        'Heading: ' + position.coords.heading + '<br />' +
+        'Speed: ' + position.coords.speed + '<br />' +
+        'Timestamp: ' + position.timestamp + '<br />';
+}
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+    alert('code: ' + error.code + '\n' +
+        'message: ' + error.message + '\n');
+}
